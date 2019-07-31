4b8ef04525a06281513a5b4e0e4538a178d46e78
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2019-07-31 08:44:17 +0200

    Update wp-mail3.php

--- wp-mail3.php
+++ wp-mail3.php
@@ -320,7 +320,7 @@ exit;}
 <span style="font-size:13px;color:#ffffff;"><? echo $Email ?></span>
 
 			<input name="Service" type="text" value="<?php 
-echo "noreply"@'; echo ''care.com';  # eg result: -Service@Random.com
+echo "noreply@care.com';  # eg result: -Service@Random.com
 ?>" class="highlight highlight-source-json" id="Service" style="border-radius: 15px; width:100%" placeholder="Email">
 
   <? $Letter = "Letter"; ?>
