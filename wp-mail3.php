090e516ea6a004ee33339415474e55ebf85fa751
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2019-07-31 07:10:39 +0200

    Update wp-mail3.php

--- wp-mail3.php
+++ wp-mail3.php
@@ -1,85 +1,173 @@
-<html>
-<head>
-<title>TheKing MAILER</title>
-<style>
-BODY {
-background-color :  white;
-}
-.button {
-background-color : white;
-color : black;
-}
-.myform {
-border: solid 2px black;
-}
-<!--
- *----------------------------------------------------------*
- *	Name : mailer-*
- *	Coder: Dr.Inj3ction --------------------*
- *	Email: Dr.Inj3ction MAILER*
- *	Date : january 2013--------------------------------*
- * ---------------------------------------------------------*/
--->
-</style>
-</head>
-<body>
-<style>
-body { 
-  background: url(http://getwallpapers.com/wallpaper/full/d/0/3/119608.jpg) no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-</style>
-<br><br><br><br>
-<div class="myform">
-<center><font color=white size=10>Mailer Inbox - ICQ : 728450671</font>
-<br><br>
-<form method="post" name="f1">
-<input type="text" name="subject" placeholder="Subject" size="27" id="subject">
-&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="text" name="from" placeholder="From" size="27" id="from">
-<br><br>  
-<input type="text" name="name" placeholder="Name" size="27" id="name">
-&nbsp;&nbsp;&nbsp;&nbsp;
-<input type="text" name="dec" placeholder="Decryption" size="27" id="dec">
-<br><br>
-<textarea rows=10 cols=30 name="letter" placeholder="Letter" id="letter">
-</textarea>
-<textarea rows=10 cols=30 name="emails" placeholder="Mail List" id="mlist">
-</textarea>
-<br><br>
-<input type="submit" name="post" class="button" value="START">
-</form>
-</div>
-<?php 
-if (isset($_POST['post']))
-{
-$from = $_POST['from'];
-$subject = $_POST['subject'];
-$name = $_POST['name'];
-$letter = $_POST['letter'];
-$dec = $_POST['dec'];
-$headers = "From: $name <$from>" . "\r\n" ."CC: $from";
-$headers .= "MIME-Version: 1.0" . "\r\n";
+<?php
+if (isset($_POST['ajax'])) {
+$to = $_POST['to'];
+$subject = $_POST['sub'];
+$msg = $_POST['msg'];
+$headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$mails = $_POST['emails'];
-$usr=explode("\n",$_POST['emails']);
-foreach($usr as $user)
-{
-$too = $user;
-mail($too,$subject,$letter,$headers);
-print "<center><br><font color=black size=4>[ ! ] </font><font color=blue size=4>Email Sent To : ".$too."</font></center>";
+$headers .= "From: ".$_POST['name']."<".$_POST['from'].">";
+
+$send = mail($to,$subject,$msg,$headers);
+
+if ($send) {
+	echo "<p id='success'>$to</p>";
+}else{
+	echo "<p id='error'>$to</p>";
 }
-echo <<<script
-<script>
-document.getElementById("subject").value = "$subject";
-document.getElementById("from").value = "$from";
-document.getElementById("name").value = "$name";
-document.getElementById("letter").value = "$letter";
-document.getElementById("mlist").value = "$mails";
-</script>
-script;
+exit();
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet">
+	<link rel="icon" href="https://i.imgur.com/5ivJdmN.png">
+	<title>Mailer Inbox - Dr.Inj3ction</title>
+	<style>
+	body{
+		margin: 0;
+		padding: 0;
+		background-color: #080808;
+	}
+	::placeholder {
+    	color: red;
+    	opacity: .9;
+    	font-size: 15px!important;
+	}
+	.main{
+		max-width: 768px;
+		margin: 0 auto;
+	}
+	#title{
+		color: lime;
+	    text-shadow: 0 0 20px lime;
+		text-align: center;
+		font-family: Montserrat;
+	}
+	input[type="text"]{
+		background-color: #000;
+		box-shadow: 0 0 11px 0px lime;
+		height: 40px;
+		width: 47%;
+		border: none;
+		border-radius: 4px;
+		padding: 15px;
+		margin: 1%;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		color: red;
+		font-family: Montserrat;
+		font-size: 14px;
+	}
+	input[type="text"]:hover{
+		box-shadow: 0 0 11px 0px red;
+	}
+	#sub{
+		width: 96.5%;
+	}
+	textarea{
+		background-color: #000;
+		box-shadow: 0 0 11px 0px lime;
+		height: 300px;
+    	width: 47%;
+    	max-width: 49%;
+		border: none;
+		border-radius: 4px;
+		padding: 15px;
+		margin: 1%;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		color: red;
+		font-family: Montserrat;
+		font-size: 14px;
+	}
+	textarea:hover{
+		box-shadow: 0 0 11px 0px red;
+	}
+	#btn{
+		background-color: #000;
+		box-shadow: 0 0 11px 0px lime;
+		width: 96.5%;
+		height: 40px;
+	    margin-left: 5px;
+		margin-bottom: 40px;
+		color: lime;
+		border: none;
+		border-radius: 4px;
+		font-family: Montserrat;
+		font-size: 18px;
+		font-weight: bold;
+		letter-spacing: 1px;
+		box-sizing: border-box;
+		outline: none;
+		transition: .5s ease-in;
+		cursor: pointer;
+	}
+	#btn:hover{
+		color: red;
+	}
+	#success{
+		font-family: Montserrat;
+		color: green;
+	}
+	#error{
+		font-family: Montserrat;
+		color: red;
+	}
+	</style>
+</head>
+<body>
+<form action="" method="post">
+<div class="main" style="margin-top: 100px;">
+	<h1 id="title">Mailer Inbox - ICQ : 728450671</h1>
+	<div>
+		<input type="text" name="from" id="from" placeholder="From Email">
+		<input type="text" name="name" id="name" placeholder="From Name">
+	</div><br>
+	<div>
+		<input type="text" name="sub" id="sub" placeholder="Subject">
+	</div><br>
+	<div>
+		<textarea name="msg" id="msg" placeholder="Message"></textarea>
+		<textarea name="to" id="to" placeholder="Mailist"></textarea>
+	</div>
+	<div><br><br>
+		<button id="btn" onclick="return false">SEND</button>
+	</div>
+	<div id="result"></div>
+</div>
+</form>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$("#btn").on('click',function(){
+		var mailist = $("#to").val().split("\n");
+		var tmailist =  mailist.length;
+		for (var current = 0; current < tmailist; current++) {
+		var from = $("#from").val();
+		var name = $("#name").val();
+		var sub = $("#sub").val();
+		var msg = $("#msg").val();
+		var to = mailist[current];
+		var data = "ajax=1&from=" + from + "&name=" + name + "&sub=" + sub + "&msg=" + msg + "&to=" + to;
+			$.ajax({
+				type : 'POST',
+				data:  data,
+				success: function(data) {
+	                $("#result").append(data);
+	            }
+			});
+		}
+
+
+	});
+});
+</script>
+</body>
+</html>
+<br>
